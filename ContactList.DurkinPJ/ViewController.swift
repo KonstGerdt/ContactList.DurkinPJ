e2464b67d6d1d1d376db915b3a70bf3d17ac5eb5
Konstantin <konstantinivanovich.d@gmail.com>
2023-03-08 17:49:35 +0300

    Initial Commit

---
+++ ContactList.DurkinPJ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList.DurkinPJ
+//
+//  Created by Konstantin Durkin on 8.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
