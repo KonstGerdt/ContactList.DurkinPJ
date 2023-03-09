cb14abddda547a8544eba047f3c4247c44a57182
Konstantin <konstantinivanovich.d@gmail.com>
2023-03-09 12:25:25 +0300

    9 march

---
+++ ContactList.DurkinPJ/ViewControllers/ContactDetailsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ContactKistDetailsViewController.swift
+//  ContactList.DurkinPJ
+//
+//  Created by Konstantin Durkin on 8.03.2023.
+//
+
+import UIKit
+
+class ContactDetailsViewController: UIViewController {
+    
+    @IBOutlet var emailLabel: UILabel!
+    @IBOutlet var phoneNumberLabel: UILabel!
+    
+    var person: Person!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = person.fullName
+        emailLabel.text = "Email: \(person.email)"
+        phoneNumberLabel.text = "Phone: \(person.phoneNumber)"
+    }
+    
+}
+
